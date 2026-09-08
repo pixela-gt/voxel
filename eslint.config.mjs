23d724bfb1dcc4bc94e067327c85f80b3b98418d
Estuardo Diaz <taro@pixela.tech>
2026-09-07 19:52:15 -0600

    fix(eslint): remove missing storybook plugin and add proper ignores
    - Remove eslint-plugin-storybook import (not installed)
    - Add ignores for node_modules, dist, .nuxt, .output, storybook-static, playground

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,10 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 import js from '@eslint/js'
 import vue from 'eslint-plugin-vue'
 import tseslint from 'typescript-eslint'
 
 export default [
+  { ignores: ['**/node_modules/**', '**/dist/**', '**/.nuxt/**', '**/.output/**', '**/storybook-static/**', '**/playground/**'] },
+
   js.configs.recommended,
   ...tseslint.configs.recommended,
   ...vue.configs['flat/recommended'],
@@ -23,5 +22,4 @@ export default [
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     },
   },
-  ...storybook.configs["flat/recommended"]
-];
+]
